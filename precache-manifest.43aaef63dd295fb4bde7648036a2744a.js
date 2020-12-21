06d2b420a50fe4dcd3cc27662781609de1222cfd
Aalya Banu <aalya.banu@gmail.com>
2020-12-21 21:51:10 +0000

    Updates

---
+++ precache-manifest.43aaef63dd295fb4bde7648036a2744a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2c8fad5d1aa0fb15dc751b43454ccb",
+    "url": "/myCalculator/index.html"
+  },
+  {
+    "revision": "48e7d2a7a969dfb707c8",
+    "url": "/myCalculator/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9c9ffff9593b871a30c7",
+    "url": "/myCalculator/static/css/main.6707fc4c.chunk.css"
+  },
+  {
+    "revision": "48e7d2a7a969dfb707c8",
+    "url": "/myCalculator/static/js/2.7c3ad38d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/myCalculator/static/js/2.7c3ad38d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9ffff9593b871a30c7",
+    "url": "/myCalculator/static/js/main.6f95b61a.chunk.js"
+  },
+  {
+    "revision": "d58556afcc6590679b4f",
+    "url": "/myCalculator/static/js/runtime-main.970318fa.js"
+  }
+]);
